b57843533dc553cba6583b7f36eb8b1d66bcb0b7
jpoist97 <jpoist97@gmail.com>
2021-11-01 00:26:47 -0700

    Install Prettier
    Added prettier and added style formatting and checking to package.json scripts

--- frontend/isoko/src/App.test.tsx
+++ frontend/isoko/src/App.test.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { mount } from '@cypress/react'
-import App from './App'
+import React from 'react';
+import { mount } from '@cypress/react';
+import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
 it('renders learn react link', () => {
-  mount(<Provider store={store}>
-    <App />
-    </Provider>)
-  cy.get('a').contains('React')
-})
+   mount(
+      <Provider store={store}>
+         <App />
+      </Provider>
+   );
+   cy.get('a').contains('React');
+});
