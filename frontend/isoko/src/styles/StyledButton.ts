7beee27b43bfa159905d7fe675c0b7f79c0221a6
reillynski <rsalkows@calpoly.edu>
2021-11-09 13:39:49 -0800

    fixed compilation issue
    needed to make "primary" prop optional

--- frontend/isoko/src/styles/StyledButton.ts
+++ frontend/isoko/src/styles/StyledButton.ts
@@ -1,14 +1,13 @@
-import styled, { StyledFunction } from 'styled-components'; 
+import styled from 'styled-components'; 
 
-type ButtonProps = {primary: boolean}
-
-
-//const button: StyledFunction<buttonProps & React.HTMLProps<HTMLButtonElement>> = styled.button
+interface ButtonProps {
+    primary?: boolean
+}
 
 const StyledButton = styled.button<ButtonProps>`
-    background: ${(props: { primary: any; }) => props.primary ? "#F97D0B" : "white"}; 
-    color: ${(props: { primary: any; }) => props.primary ? "white" : "#F97D0B"}; 
-    box-shadow: ${(props: { primary: any; }) => props.primary ? "0px 4px 4px rgba(0, 0, 0, 0.25)" : 
+    background: ${props => props.primary ? "#F97D0B" : "white"}; 
+    color: ${props => props.primary ? "white" : "#F97D0B"}; 
+    box-shadow: ${props => props.primary ? "0px 4px 4px rgba(0, 0, 0, 0.25)" : 
         "0px 0px 1px rgba(0, 0, 0, 0.25), 0px 2px 4px rgba(0, 0, 0, 0.11)"};
     font-size: 0.75em; 
     font-family: Open Sans;
