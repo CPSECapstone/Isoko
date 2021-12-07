23a41c33409a0f26a0eb7576e7ca5df26c383f1d
Shriya <shriyan44@gmail.com>
2021-12-07 12:45:46 -0800

    deleted gitignore and wrote trivial test

---
+++ backend/isoko-backend/__tests__/unit/handlers/trivial.test.js
@@ -0,0 +1,12 @@
+describe('Test nothing', () => { 
+ 
+    it('should test nothing', async () => { 
+        result = 1
+ 
+        const expectedResult = 1
+ 
+        // Compare the result with the expected result 
+        expect(result).toEqual(expectedResult); 
+    }); 
+}); 
+ 
