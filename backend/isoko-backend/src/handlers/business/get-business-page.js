2396d19cc6ec2b7dc5f065bb6f509dfd04377ad7
jpoist97 <jpoist97@gmail.com>
2022-01-15 12:34:34 -0800

    Implemented Business Search Endpoint
    Implemented business search endpoint and added prettier to backend for code styling.

--- backend/isoko-backend/src/handlers/business/get-business-page.js
+++ backend/isoko-backend/src/handlers/business/get-business-page.js
@@ -1,19 +1,24 @@
-
 /**
  * HTTP get method to get business page with details and reviews for specific businessId.
  */
 exports.getBusinessPageHandler = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getBusinessPage only accept GET method, you tried: ${event.httpMethod}`);
-    }
+   if (event.httpMethod !== 'GET') {
+      throw new Error(
+         `getBusinessPage only accept GET method, you tried: ${event.httpMethod}`
+      );
+   }
 
-    console.info('received:', event);
+   console.info('received:', event);
 
-    const response = {
-        statusCode: 200,
-        body: {...JSON.parse(event.body), requestParams: event.pathParameters }       
-    };
+   const response = {
+      statusCode: 200,
+      body: { ...JSON.parse(event.body), requestParams: event.pathParameters },
+   };
 
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${JSON.stringify(response.body)}`);
-    return response;
-}
+   console.info(
+      `response from: ${event.path} statusCode: ${
+         response.statusCode
+      } body: ${JSON.stringify(response.body)}`
+   );
+   return response;
+};
--- backend/isoko-backend/src/handlers/flag/get-review-flags.js
+++ backend/isoko-backend/src/handlers/flag/get-review-flags.js
@@ -1,19 +1,24 @@
-
 /**
  * HTTP get method that lists all the flags for the specified review.
  */
 exports.getReviewFlagsHandler = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getReviewFlags only accept GET method, you tried: ${event.httpMethod}`);
-    }
+   if (event.httpMethod !== 'GET') {
+      throw new Error(
+         `getReviewFlags only accept GET method, you tried: ${event.httpMethod}`
+      );
+   }
 
-    console.info('received:', event);
+   console.info('received:', event);
 
-    const response = {
-        statusCode: 200,
-        body: {...JSON.parse(event.body), requestParams: event.pathParameters }       
-    };
+   const response = {
+      statusCode: 200,
+      body: { ...JSON.parse(event.body), requestParams: event.pathParameters },
+   };
 
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${JSON.stringify(response.body)}`);
-    return response;
-}
+   console.info(
+      `response from: ${event.path} statusCode: ${
+         response.statusCode
+      } body: ${JSON.stringify(response.body)}`
+   );
+   return response;
+};
