75d95063039b536fda3821e666ed9d6ddefebe7e
reillynski <rsalkows@calpoly.edu>
2022-01-19 10:24:06 -0800

    POST /business/ and tests

--- backend/isoko-backend/src/handlers/business/post-list-business.js
+++ backend/isoko-backend/src/handlers/business/post-list-business.js
@@ -1,3 +1,8 @@
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const _ = require('lodash');
+const docClient = new dynamodb.DocumentClient();
+const { BUSINESS_TABLE } = require('../../constants');
+
 /**
  * HTTP post method to allow a user to list a new business.
  */
@@ -9,10 +14,78 @@ exports.postListBusinessHandler = async (event) => {
    }
 
    console.info('received:', event);
+   const requestBody = event.body && JSON.parse(event.body);
+   
+   const name = _.get(requestBody, 'name');
+   // add defaultVal param bc city is optional 
+   const city = _.get(requestBody, 'city', "");
+   const type = _.get(requestBody, 'type');
+   const tags = _.get(requestBody, 'tags');
+   const keywords = _.get(requestBody, 'keywords');
+   const shortDesc = _.get(requestBody, 'shortDesc');
+   const businessId = _.get(requestBody, 'businessId');
+   const hours = _.get(requestBody, 'hours');
+   const links = _.get(requestBody, 'links');
+   const address = _.get(requestBody, 'address');
+   const owner = _.get(requestBody, 'owner', "");
+   const ownerName = _.get(requestBody, 'ownerName', ""); 
+   const ownerPhone = _.get(requestBody, 'ownerPhone', "");
+   const ownerDesc = _.get(requestBody, 'ownerDesc', "");
+   const photo = _.get(requestBody, 'photo', "");
+   const lister = _.get(requestBody, 'lister');
+   
+
+   const queryParams = {
+      TableName: BUSINESS_TABLE,
+      Item: {
+         "name": name, 
+         "city": city,
+         "type": type, 
+         "tags": tags, 
+         "keywords": keywords,
+         "shortDesc": shortDesc, 
+         "businessId": businessId, 
+         "hours": hours, 
+         "links": links, 
+         "address": address, 
+         "owner": owner,
+         "ownerName": ownerName, 
+         "ownerPhone": ownerPhone, 
+         "ownerDesc": ownerDesc,
+         "photo": photo, 
+         "lister": lister
+      }
+   }
+
+   // remove optional params if not present
+   if (city == "") {
+      delete queryParams.Item.city; 
+   }
+   
+   // if request is coming from non-owner list a business page, following params will not be included 
+   if (owner == "") {
+         delete queryParams.Item.owner; 
+         delete queryParams.Item.ownerName;
+         delete queryParams.Item.ownerPhone; 
+         delete queryParams.Item.ownerDesc; 
+         delete queryParams.Item.photo; 
+         delete queryParams.Item.shortDesc; 
+         delete queryParams.Item.hours; 
+         delete queryParams.Item.links; 
+   }
+
+   
+   const dynamoResult = await docClient
+      .put(queryParams) 
+      .promise(); 
+   
+   let putResult = dynamoResult.Items; 
 
    const response = {
       statusCode: 200,
-      body: { ...JSON.parse(event.body) },
+      body: { 
+         results: putResult
+       },
    };
 
    console.info(
