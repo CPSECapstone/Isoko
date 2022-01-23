92a7e655c7cb66da5f9226318192021630d6c28d
Rohith Dara <rohith.dara2000@gmail.com>
2022-01-22 22:08:33 -0800

    #time 2m ran prettier formatter

--- backend/isoko-backend/src/handlers/review/post-review.js
+++ backend/isoko-backend/src/handlers/review/post-review.js
@@ -1,4 +1,4 @@
-const dynamodb = require('aws-sdk/clients/dynamodb')
+const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 const _ = require('lodash');
 const { BUSINESS_TABLE } = require('../../constants');
@@ -6,64 +6,63 @@ const { BUSINESS_TABLE } = require('../../constants');
 /**
  * HTTP post method that creates a review for the specified business.
  */
- exports.postReviewHandler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-       throw new Error(
-          `postReviewHandler only accept POST method, you tried: ${event.httpMethod}`
-       );
-    }
- 
-    console.info('received:', event);
+exports.postReviewHandler = async (event) => {
+   if (event.httpMethod !== 'POST') {
+      throw new Error(
+         `postReviewHandler only accept POST method, you tried: ${event.httpMethod}`
+      );
+   }
 
-    const { businessId } = event.pathParameters;
+   console.info('received:', event);
 
-    if (businessId == null) {
-        throw new Error(
-            `Missing query parameter 'businessId'. Request URL format: POST/business/:businessId/review`
-        );
-    }
+   const { businessId } = event.pathParameters;
 
-    const requestBody = event.body && JSON.parse(event.body);
+   if (businessId == null) {
+      throw new Error(
+         `Missing query parameter 'businessId'. Request URL format: POST/business/:businessId/review`
+      );
+   }
 
-    const reviewAuthor = _.get(requestBody, 'reviewAuthor');
-    const authorUserName = _.get(requestBody, 'authorUserName');
-    const authorProfilePicture = _.get(requestBody, 'authorProfilePicture', "");
-    const stars = _.get(requestBody, 'stars');
-    const reviewTitle = _.get(requestBody, 'reviewTitle', "");
-    const description = _.get(requestBody, 'description', "");
-    const pictures = _.get(requestBody, 'pictures', []);
-    const ts = _.get(requestBody, 'ts');
-    
-    const params = {
-        TableName: BUSINESS_TABLE,
-        Item: {
-            pk: businessId,
-            sk: "REVIEW#" + ts + "#" + authorUserName,
-            reviewAuthor: reviewAuthor,
-            authorUserName: authorUserName,
-            authorProfilePicture: authorProfilePicture,
-            stars: stars,
-            reviewTitle: reviewTitle,
-            description: description,
-            pictures: pictures,
-            ts: ts,
-        }
-    }
+   const requestBody = event.body && JSON.parse(event.body);
 
-    const dynamoResult = await docClient.put(params).promise();
+   const reviewAuthor = _.get(requestBody, 'reviewAuthor');
+   const authorUserName = _.get(requestBody, 'authorUserName');
+   const authorProfilePicture = _.get(requestBody, 'authorProfilePicture', '');
+   const stars = _.get(requestBody, 'stars');
+   const reviewTitle = _.get(requestBody, 'reviewTitle', '');
+   const description = _.get(requestBody, 'description', '');
+   const pictures = _.get(requestBody, 'pictures', []);
+   const ts = _.get(requestBody, 'ts');
 
-    let putResult = dynamoResult.Items;
+   const params = {
+      TableName: BUSINESS_TABLE,
+      Item: {
+         pk: businessId,
+         sk: 'REVIEW#' + ts + '#' + authorUserName,
+         reviewAuthor: reviewAuthor,
+         authorUserName: authorUserName,
+         authorProfilePicture: authorProfilePicture,
+         stars: stars,
+         reviewTitle: reviewTitle,
+         description: description,
+         pictures: pictures,
+         ts: ts,
+      },
+   };
 
-    const response = {
-       statusCode: 200,
-       body: { results: putResult },
-    };
- 
-    console.info(
-       `response from: ${event.path} statusCode: ${
-          response.statusCode
-       } body: ${JSON.stringify(response.body)}`
-    );
-    return response;
- };
- 
+   const dynamoResult = await docClient.put(params).promise();
+
+   let putResult = dynamoResult.Items;
+
+   const response = {
+      statusCode: 200,
+      body: { results: putResult },
+   };
+
+   console.info(
+      `response from: ${event.path} statusCode: ${
+         response.statusCode
+      } body: ${JSON.stringify(response.body)}`
+   );
+   return response;
+};
--- backend/isoko-backend/src/handlers/triggers/cognito-post-confirmation.js
+++ backend/isoko-backend/src/handlers/triggers/cognito-post-confirmation.js
@@ -6,21 +6,24 @@ const { USER_TABLE } = require('../../constants');
  * Creates a new user in dynamodb after they confirm their email.
  */
 exports.cognitoPostConfirmationHandler = async (event, context, callback) => {
-    console.log('received: ', event);
-    
-    const params = {
-        Item: {
-            "pk": event.request.userAttributes.sub,
-            "name": event.request.userAttributes.name + " " + event.request.userAttributes.family_name,
-            "email": event.request.userAttributes.email,
-            "profilePicture":"",
-            "businessOwner": false,
-            "businessId": ""
-        },
-        ReturnConsumedCapacity: "TOTAL",
-        TableName: USER_TABLE
-    };
-    await docClient.put(params).promise();
+   console.log('received: ', event);
 
-    callback(null, event);
+   const params = {
+      Item: {
+         pk: event.request.userAttributes.sub,
+         name:
+            event.request.userAttributes.name +
+            ' ' +
+            event.request.userAttributes.family_name,
+         email: event.request.userAttributes.email,
+         profilePicture: '',
+         businessOwner: false,
+         businessId: '',
+      },
+      ReturnConsumedCapacity: 'TOTAL',
+      TableName: USER_TABLE,
+   };
+   await docClient.put(params).promise();
+
+   callback(null, event);
 };
