50ac7a4e917317edd00a50e6be87b4466fb58b5f
jpoist97 <jpoist97@gmail.com>
2022-01-31 18:18:21 -0800

    Demo of carousel

--- frontend/isoko/src/pages/Business.tsx
+++ frontend/isoko/src/pages/Business.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Review from '../components/Review';
 import BusinessHeader from '../components/business/BusinessHeader';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const Title = styled.h1`
    font-size: 2.5em;
@@ -9,59 +11,46 @@ const Title = styled.h1`
    padding: 0em 0em 0em 0.5em;
 `;
 
+const Photo = styled.img`
+   height: 140px;
+   width: 140px;
+   margin: auto 0;
+   border-radius: 5px;
+   object-fit: cover;
+   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const responsive = {
+   superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+   },
+   desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+   },
+   tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+   },
+   mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+   },
+};
+
 const Business = () => (
    <main>
       <h1>Business Listing</h1>
-      <BusinessHeader
-         name="Bob's Burgers"
-         description="Bob rallies the team to cook up some business, so he sends middle child Gene to push something or other here this is long"
-         stars={3}
-         minorityTags={['Black-Owned']}
-         keywordTags={['Burgers']}
-         verified={true}
-         numReviews={80}
-      />
-      <Title>Ratings & Reviews</Title>
-      <Review
-         reviewerName="Sir Theodore"
-         reviewerImageUrl="https://www.pluggedin.com/wp-content/uploads/2020/01/bobs-burgers-review-image.jpg"
-         stars={4}
-         subject="Great food and service!"
-         content="I've never regretted eating at Bill's Diner. The Signature Burger is to die for and Bill is a homie. Everyone should come and eat some of Bill's food made with love and care."
-         imageUrls={[
-            'https://images.unsplash.com/photo-1587410131477-f01b22c59e1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGFsbCUyMHRvd2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
-            'https://www.pluggedin.com/wp-content/uploads/2020/01/bobs-burgers-review-image.jpg',
-            'https://www.robin-noorda.com/uploads/1/6/8/3/16830688/3347022_orig.jpg',
-         ]}
-      />
-      <Review
-         reviewerName="Sir Theodore"
-         reviewerImageUrl="https://www.pluggedin.com/wp-content/uploads/2020/01/bobs-burgers-review-image.jpg"
-         stars={4}
-         subject="Great food and service!"
-         content="I've never regretted eating at Bill's Diner. The Signature Burger is to die for and Bill is a homie. Everyone should come and eat some of Bill's food made with love and care."
-         imageUrls={[
-            'https://www.pluggedin.com/wp-content/uploads/2020/01/bobs-burgers-review-image.jpg',
-            'https://static3.srcdn.com/wordpress/wp-content/uploads/2020/02/Arrested-Development-Banana-Stand.jpg',
-         ]}
-      />
-      <Review
-         reviewerName="Sir Theodore"
-         reviewerImageUrl="https://www.pluggedin.com/wp-content/uploads/2020/01/bobs-burgers-review-image.jpg"
-         stars={4}
-         subject="Great food and service!"
-         content="I've never regretted eating at Bill's Diner. The Signature Burger is to die for and Bill is a homie. Everyone should come and eat some of Bill's food made with love and care."
-         imageUrls={[
-            'https://www.pluggedin.com/wp-content/uploads/2020/01/bobs-burgers-review-image.jpg',
-         ]}
-      />
-      <Review
-         reviewerName="Sir Theodore"
-         reviewerImageUrl="https://www.pluggedin.com/wp-content/uploads/2020/01/bobs-burgers-review-image.jpg"
-         stars={4}
-         subject="Great food and service!"
-         content="I've never regretted eating at Bill's Diner. The Signature Burger is to die for and Bill is a homie. Everyone should come and eat some of Bill's food made with love and care."
-      />
+      <Carousel responsive={responsive}>
+         <Photo src="https://picsum.photos/id/1018/1000/600/" />
+         <Photo src="https://picsum.photos/id/1015/1000/600/" />
+         <Photo src="https://picsum.photos/id/1019/1000/600/" />
+         <Photo src="https://picsum.photos/id/1018/1000/600/" />
+         <Photo src="https://picsum.photos/id/1015/1000/600/" />
+         <Photo src="https://picsum.photos/id/1019/1000/600/" />
+      </Carousel>
    </main>
 );
 
