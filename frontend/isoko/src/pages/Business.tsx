edbd6da102dda5b6ac42b7534e7ca1077ff4d061
Rohith Dara <rohith.dara2000@gmail.com>
2022-02-08 14:22:45 -0800

    finished

--- frontend/isoko/src/pages/Business.tsx
+++ frontend/isoko/src/pages/Business.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Review from '../components/Review';
 import BusinessHeader from '../components/business/BusinessHeader';
+import AboutTheOwner from '../components/business/AboutTheOwner';
 
 const Title = styled.h1`
    font-size: 2.5em;
@@ -14,12 +15,23 @@ const Business = () => (
       <h1>Business Listing</h1>
       <BusinessHeader
          name="Bob's Burgers"
-         description="Bob rallies the team to cook up some business, so he sends middle child Gene to push something or other here this is long"
+         description="Bob rallies the team to cook up some business, so he sends middle child Gene to push something or other here this is long this is long this is long this is long this is long this is long this is long this is long this is long"
          stars={3}
          minorityTags={['Black-Owned']}
          keywordTags={['Burgers']}
          verified={true}
          numReviews={80}
+      />
+      <Title>About the Owner</Title>
+      <AboutTheOwner
+         ownerImageUrl='https://www.pluggedin.com/wp-content/uploads/2020/01/bobs-burgers-review-image.jpg'
+         content='My name is Bill and I am the proud owner of Bill’s Diner. 
+         For the past 15 years I’ve been in the restuarant industry and 
+         I’ve always been a proud black business owner. As a black 
+         business owner some of the things I value is seving authentic 
+         and delicious food to the great community here in San Luis Obispo. 
+         Come on down to Bill’s Diner and don’t be afraid to say hi if 
+         you see me there!'
       />
       <Title>Ratings & Reviews</Title>
       <Review
