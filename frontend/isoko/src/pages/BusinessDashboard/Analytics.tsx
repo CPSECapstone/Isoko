67d7c2bb25474bf1b079a0301f10688af47a9c38
jpoist97 <jpoist97@gmail.com>
2022-02-18 10:25:41 -0800

    Skeleton for business dashboard

---
+++ frontend/isoko/src/pages/BusinessDashboard/Analytics.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Analytics: React.FC = () => (
+   <main>
+      <h1>Business Dashboard Analytics</h1>
+   </main>
+);
+
+export default Analytics;
---
+++ frontend/isoko/src/pages/BusinessDashboard/Preview.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Preview: React.FC = () => (
+   <main>
+      <h1>Business Dashboard Preview</h1>
+   </main>
+);
+
+export default Preview;
