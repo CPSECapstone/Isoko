aba74d96547e3d2e22a9e818151422b647bf823b
Rohith Dara <rohith.dara2000@gmail.com>
2022-02-24 00:43:18 -0800

    first draft

---
+++ frontend/isoko/src/components/business_dashboard/SortReviewsDropdown.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+
+const StyledDropdownButton = styled(DropdownButton)`
+   background: #fff;
+   color: #000;
+   text-decoration: underline;
+   font-size: 0.5rem;
+   .btn-primary {
+      background-color: #c4c4c4;
+      color: black;
+      border-color: #c4c4c4;
+      border-radius: 5px;
+      padding: 0px;
+      font-size: 0.75rem;
+      &:focus {
+         background-color: #c4c4c4;
+         color: black;
+         border-color: #c4c4c4;
+      }
+      &:hover {
+         box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.25);
+      }
+      &.dropdown-toggle:focus {
+         box-shadow: none;
+      }
+   }
+   .btn {
+      text-decoration: underline;
+   }
+`;
+
+interface SortDropdownProps extends React.HTMLProps<HTMLDivElement> {
+   sortFunction: (key: string) => void;
+}
+
+const SortResultsDropdown: React.FC<SortDropdownProps> = (props) => {
+   const [sortKey, setSortKey] = useState('Recently Added');
+
+   return (
+      <StyledDropdownButton className={props.className} title={sortKey}>
+         <Dropdown.Item as="button">
+            <div
+               onClick={(e) => {
+                  setSortKey(e.target.textContent);
+                  props.sortFunction('recent');
+               }}
+            >
+               Recently Added
+            </div>
+         </Dropdown.Item>
+         <Dropdown.Item as="button">
+            <div
+               onClick={(e) => {
+                  setSortKey(e.target.textContent);
+                  props.sortFunction('highestRated');
+               }}
+            >
+               Highest Rated
+            </div>
+         </Dropdown.Item>
+         <Dropdown.Item as="button">
+            <div
+               onClick={(e) => {
+                  setSortKey(e.target.textContent);
+                  props.sortFunction('lowestRated');
+               }}
+            >
+               Lowest Rated
+            </div>
+         </Dropdown.Item>
+      </StyledDropdownButton>
+   );
+};
+
+export default SortResultsDropdown;
