7c36c1e67fb0a04bba572a4c94e717c1df1ac793
Shriya <shriyan44@gmail.com>
2022-02-27 20:32:46 -0800

    put progress

--- backend/isoko-backend/src/handlers/user/put-user-object.js
+++ backend/isoko-backend/src/handlers/user/put-user-object.js
@@ -1,3 +1,6 @@
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+const { USER_TABLE } = require('../../constants');
 /**
  * HTTP put method t that allows the user object specified to be updated according to the request body.
 
@@ -10,11 +13,38 @@ exports.putUserObjectHandler = async (event) => {
    }
 
    console.info('received:', event);
+   const { userSub } = event.pathParameters;
 
-   const response = {
-      statusCode: 200,
-      body: { ...JSON.parse(event.body), requestParams: event.pathParameters },
+   if (userSub == null) {
+      throw new Error(
+         `Missing query parameter 'userSub'. Request URL format: GET/user/{userSub}`
+      );
+   }
+
+   const params = {
+      TableName: USER_TABLE,
+      Key: {
+         pk: userSub,
+      },
    };
+
+   let response;
+
+   try {
+      const dynamoResult = await docClient.get(params).promise();
+
+      let getResults = dynamoResult.Item;
+
+      response = {
+         statusCode: 200,
+         body: { results: getResults },
+      };
+   } catch (e) {
+      response = {
+         statusCode: 400,
+         body: { error: e },
+      };
+   }
 
    console.info(
       `response from: ${event.path} statusCode: ${
