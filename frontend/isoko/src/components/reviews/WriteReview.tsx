cb650144108683ada3baf4af5d30ca46bfc3d954
Rohith Dara <rohith.dara2000@gmail.com>
2022-03-07 23:21:44 -0800

    Add Everything but Image Upload

---
+++ frontend/isoko/src/components/reviews/WriteReview.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+
+const WriteReviewContainer = styled.div`
+   display: flex;
+   padding: 4px 5px 4px 5px;
+   background-color: #fff;
+   text-align: center;
+   border-radius: 4px;
+   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.25), 0px 2px 4px rgba(0, 0, 0, 0.11);
+   cursor: pointer;
+   width: 115px;
+`;
+
+const StyledText = styled.p`
+   font-weight: 500;
+   margin-bottom: 0px;
+   margin-left: 5px;
+   font-family: 'Inter', sans-serif;
+   font-size: 1em;
+`;
+
+const WriteReview: React.FC = (props) => {
+   return (
+      <WriteReviewContainer className={props.className}>
+         <div>
+            <FontAwesomeIcon icon={faPencilAlt} color="#000" />
+         </div>
+         <StyledText>Write a Review</StyledText>
+      </WriteReviewContainer>
+   );
+};
+
+export default WriteReview;
