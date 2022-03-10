245c64cbd6f0a301cb3b0856bf2423e7150e3ece
jpoist97 <jpoist97@gmail.com>
2022-03-09 20:57:35 -0800

    Complete list business endpoint
    Added posting of search results record to list business endpoint

--- backend/isoko-backend/src/constants.js
+++ backend/isoko-backend/src/constants.js
@@ -1,2 +1,6 @@
 exports.USER_TABLE = 'Users';
 exports.BUSINESS_TABLE = 'Businesses';
+exports.SEARCH_RESULTS_TABLE = 'SearchResults';
+
+exports.BRICK_AND_MORTAR = 'B&M';
+exports.ONLINE = 'Online';
