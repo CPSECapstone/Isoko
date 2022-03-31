76080e8ae67f759299ed1b8ed60a30042fbcc149
jpoist97 <jpoist97@gmail.com>
2022-03-31 16:12:47 -0700

    Add slice to store

--- frontend/isoko/src/app/store.ts
+++ frontend/isoko/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import searchResultsReducer from '../features/business/SearchResultsSlice';
+import dashboardReducer from '../features/dashboard/DashboardSlice';
 
 export const store = configureStore({
    reducer: {
       counter: counterReducer,
       searchResults: searchResultsReducer,
+      dashboard: dashboardReducer,
    },
 });
 
