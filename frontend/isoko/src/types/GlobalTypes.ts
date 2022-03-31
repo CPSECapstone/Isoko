9745cfb9e96105365d556087035f6cf9ce959582
jpoist97 <jpoist97@gmail.com>
2022-03-31 14:54:55 -0700

    Implemented reducers according to spec

--- frontend/isoko/src/types/GlobalTypes.ts
+++ frontend/isoko/src/types/GlobalTypes.ts
@@ -13,3 +13,60 @@ export interface BusinessPreview {
    zipCode: number;
    timestamp: number;
 }
+
+export enum Day {
+   Mon = 'Mon',
+   Tue = 'Tue',
+   Wed = 'Wed',
+   Thu = 'Thu',
+   Fri = 'Fri',
+   Sat = 'Sat',
+   Sun = 'Sun',
+}
+
+export interface Hours {
+   [Day.Mon]?: string;
+   [Day.Tue]?: string;
+   [Day.Wed]?: string;
+   [Day.Thu]?: string;
+   [Day.Fri]?: string;
+   [Day.Sat]?: string;
+   [Day.Sun]?: string;
+}
+
+export interface Review {
+   reviewAuthor: string;
+   authorUserName: string;
+   authorProfilePicture: string;
+   rating: number;
+   reviewTitle?: string;
+   description: string;
+   pictures: Array<string>;
+   ts: number;
+}
+
+export interface Business {
+   name: string;
+   city?: string;
+   type: 'B&M' | 'Online';
+   tags: Array<string>;
+   keywords: Array<string>;
+   rating: number;
+   shortDesc: string;
+   numReviews: number;
+   claimed: boolean;
+   businessId: string;
+   hours: Hours;
+   links: {
+      [key: string]: string;
+   };
+   address: string;
+   aboutOwner?: {
+      owner: string;
+      ownerName: string;
+      ownerPhone: string;
+      ownerDesc: string;
+      photo: string;
+   };
+   reviews: Array<Review>;
+}
