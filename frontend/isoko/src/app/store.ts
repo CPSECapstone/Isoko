a88303adc1896b6fc4ee2e0b726ec701ee7a38e6
Rohith Dara <rohith.dara2000@gmail.com>
2022-04-12 14:15:54 -0700

    IS-110 Business Slice Redux (#71)
    business slice redux

--- frontend/isoko/src/app/store.ts
+++ frontend/isoko/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import searchResultsReducer from '../features/business/SearchResultsSlice';
+import businessReducer from '../features/business/BusinessSlice';
 
 export const store = configureStore({
    reducer: {
       counter: counterReducer,
       searchResults: searchResultsReducer,
+      business: businessReducer,
    },
 });
 
