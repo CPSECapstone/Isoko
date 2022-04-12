dda2303c154aa5c0932ab91224233d45354bc24b
Rohith Dara <rohith.dara2000@gmail.com>
2022-04-12 12:42:28 -0700

    needs tests

--- frontend/isoko/src/features/business/BusinessAPI.ts
+++ frontend/isoko/src/features/business/BusinessAPI.ts
@@ -2,35 +2,11 @@ import axios from 'axios';
 import { environment } from '../../environment/environment';
 import { Business } from '../../types/GlobalTypes';
 
-export const fetchSearchResults = async (
-   businessId: string
-): Promise<Array<Business>> => {
+export const fetchBusiness = async (businessId: string): Promise<Business> => {
    const response = await axios.get(
       `${environment.prodURL}/business/${businessId}`
    );
 
-   const searchResults = response.data.results;
+   const business = response.data.results;
    return business as Business;
-};
-
-export const getSearchParams = (
-   location: string,
-   minorityTags: Array<string>,
-   searchTerm: string
-): SearchParams | null => {
-   const locationSplit = location.split(', ');
-
-   // Error checking for location without all the information
-   if (locationSplit.length < 3) {
-      alert('Invalid search location, please try another location');
-      return null;
-   }
-
-   return {
-      location: `${locationSplit[1]}#${locationSplit[0]}`,
-      tags: minorityTags.includes('Any Minority Owned') ? [] : minorityTags,
-      ...(categoryList.includes(searchTerm)
-         ? { category: searchTerm }
-         : { keyword: searchTerm }),
-   };
 };
