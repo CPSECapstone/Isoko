3ae93adbb3dafe2116e13d5df62323bac8894c46
Cole Perry <ctperry0301@gmail.com>
2022-04-19 14:35:40 -0700

    Styling Consistencies, fixed auth inputs to be same as elsewhere

--- frontend/isoko/src/pages/Authentication/Login.tsx
+++ frontend/isoko/src/pages/Authentication/Login.tsx
@@ -9,6 +9,7 @@ import {
    CognitoUserPool,
 } from 'amazon-cognito-identity-js';
 import { environment } from '../../environment/environment';
+import { Form } from 'react-bootstrap';
 
 const LeftDiv = styled.div`
    width: 50%;
@@ -70,13 +71,23 @@ const InputContainer = styled.div`
    width: 70%;
 `;
 
-const StyledInput = styled.input`
+const StyledInput = styled(Form.Control)`
    width: 100%;
+   font-size: 1.0625em;
+   padding: 0px 12px 0px 4px;
    border-radius: 10px;
    border: none;
    height: 32px;
    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
    text-indent: 10px;
+
+   &:focus {
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      border: 1px solid rgb(249, 125, 11);
+   }
+   ::placeholder {
+      color: #aaaaaa;
+   }
 `;
 
 const StyledLabel = styled.label`
